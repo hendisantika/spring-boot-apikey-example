dcfcb5896a376e02dc9b56fa1badd9a66ff99731
hendisantika <hendisantika@yahoo.co.id>
2023-12-30 17:33:12 +0700

    Add doFilterInternal in ApiKeyAuthFilter 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/springbootapikeyexample/auth/ApiKeyAuthFilter.java
+++ src/main/java/com/hendisantika/springbootapikeyexample/auth/ApiKeyAuthFilter.java
@@ -1,8 +1,15 @@
 package com.hendisantika.springbootapikeyexample.auth;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +27,10 @@ public class ApiKeyAuthFilter extends OncePerRequestFilter {
 
     private final ApiKeyAuthExtractor extractor;
 
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        extractor.extract(request)
+                .ifPresent(SecurityContextHolder.getContext()::setAuthentication);
+        filterChain.doFilter(request, response);
+    }
 }
