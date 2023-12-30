447bd8bb727ee10d25c73adf32317e741078a4f6
hendisantika <hendisantika@yahoo.co.id>
2023-12-30 09:24:46 +0700

    Add authenticate in ApiKeyAuthManager 🫸🌀✏️📗 :octocat:

--- src/main/java/com/hendisantika/springbootapikeyexample/auth/ApiKeyAuthManager.java
+++ src/main/java/com/hendisantika/springbootapikeyexample/auth/ApiKeyAuthManager.java
@@ -4,6 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.cglib.core.internal.LoadingCache;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 
 import javax.sql.DataSource;
 import java.util.concurrent.TimeUnit;
@@ -28,4 +31,17 @@ public class ApiKeyAuthManager implements AuthenticationManager {
                 .expireAfterAccess(5, TimeUnit.MINUTES)
                 .build(new DatabaseCacheLoader(dataSource));
     }
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String principal = (String) authentication.getPrincipal();
+
+        if (!keys.get(principal)) {
+            throw new BadCredentialsException("The API key was not found or not the expected value.");
+        } else {
+            authentication.setAuthenticated(true);
+            return authentication;
+        }
+    }
+
 }
