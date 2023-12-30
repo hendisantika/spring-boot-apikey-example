72fec16c2ef22fddb54cbf296b10e9abf8460ae9
hendisantika <hendisantika@yahoo.co.id>
2023-12-30 17:35:10 +0700

    Add GreetingsController 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/com/hendisantika/springbootapikeyexample/controller/GreetingsController.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootapikeyexample.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-apikey-example
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/30/23
+ * Time: 17:34
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+public class GreetingsController {
+
+}
