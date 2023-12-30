d42e6cff3a05bbd8e5ea449232773f7045b7afbe
hendisantika <hendisantika@yahoo.co.id>
2023-12-31 05:59:35 +0700

    Add shouldGetPublicGreetings in GreetingsControllerTest 🫸🌀✏️📗 :octocat:

--- src/test/java/com/hendisantika/springbootapikeyexample/controller/GreetingsControllerTest.java
+++ src/test/java/com/hendisantika/springbootapikeyexample/controller/GreetingsControllerTest.java
@@ -1,10 +1,16 @@
 package com.hendisantika.springbootapikeyexample.controller;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +31,10 @@ class GreetingsControllerTest {
     @Value("${application.security.api-key}")
     private String apiKey;
 
+    @Test
+    void shouldGetPublicGreetings() throws Exception {
+        this.mockMvc.perform(get("/public/greetings"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Greetings")));
+    }
 }
