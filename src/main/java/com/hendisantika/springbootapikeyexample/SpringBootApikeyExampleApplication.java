1fac6521be963374e03e02c584885d9cc8150b9b
hendisantika <hendisantika@yahoo.co.id>
2023-12-30 07:07:07 +0700

    Create starter project 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/com/hendisantika/springbootapikeyexample/SpringBootApikeyExampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootapikeyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApikeyExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApikeyExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootapikeyexample/SpringBootApikeyExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootapikeyexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApikeyExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
